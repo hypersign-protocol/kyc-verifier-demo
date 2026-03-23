30b7d8f85e800691455dd8cb57b514d0acea386f
varsha <91577031+varsha766@users.noreply.github.com>
2026-03-23 15:41:43 +0530

    Sync develop with master (#24)
    * [CHORE]: add env
    
    * [CHORE]:UPDATE envs
    
    * Update env variable
    
    * fix:netlify
    
    * Update the screen size
    
    * Converted entire code to vue using vue script injection
    
    * fix
    
    * update
    
    * update code
    
    * added decdoing the path and fetch serviceId and path
    
    * fix
    
    * Implemeted/decoding (#19)
    
    * loader
    
    * update style
    
    * fix spinner
    
    * Fix minor bug (#22)
    
    * loader
    
    * update style
    
    * fix spinner
    
    * updated page title
    
    ---------
    
    Co-authored-by: Pratap2018 <pratapmridha@gmail.com>
    
    * Implemeted/decoding (#23)
    
    * loader
    
    * update style
    
    * fix spinner
    
    * updated page title (#21)
    
    ---------
    
    Co-authored-by: varsha <91577031+varsha766@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pratap2018 <pratapmridha@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express();
 
-app.use(express.static('public'))
+// Serve static assets (CSS, JS, images, etc.) but exclude HTML files
+app.use(express.static('public', {
+    index: false,
+    extensions: ['css', 'js', 'png', 'jpg', 'jpeg', 'gif', 'ico', 'svg', 'json']
+}))
 
+// Function to inject environment variables into HTML
+function injectEnvVariables(htmlContent) {
+    const entityDashboardBaseUrl = process.env.HYPERSIGN_DASHBOARD_SERVICE_BASE_URL || 'https://api.entity.dashboard.hypersign.id';
+    const idDashboardServiceBaseUrl = process.env.ID_SERVICE_BASE_URL || 'https://api.cavach.hypersign.id';
+    const widgetUrl = process.env.WIDGET_URL || 'https://verify.hypersign.id/';
+    
+    // Inject environment variables as window properties at the start of the script
+    const envScript = `
+    <script>
+        window.HYPERSIGN_DASHBOARD_SERVICE_BASE_URL = '${entityDashboardBaseUrl}';
+        window.ID_SERVICE_BASE_URL = '${idDashboardServiceBaseUrl}';
+        window.WIDGET_URL = '${widgetUrl}';
+    </script>`;
+    
+    // Inject before the closing </head> tag
+    let processedHtml = htmlContent.replace('</head>', `${envScript}\n</head>`);
+    
+    return processedHtml;
+}
+
+// Serve index.html with environment variables injected
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    fs.readFile(htmlPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error loading page');
+        }
+        const processedHtml = injectEnvVariables(data);
+        res.send(processedHtml);
+    });
 })
+
 app.get('/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    fs.readFile(htmlPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error loading page');
+        }
+        const processedHtml = injectEnvVariables(data);
+        res.send(processedHtml);
+    });
 });
 
 app.get('/home', (req, res) => {
